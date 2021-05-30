e896648c8d8d3c8125fc7bbc371588d2c18c9344
1Coold1 <72026831+1Coold1@users.noreply.github.com>
2021-05-30 23:46:34 +0500

    Add files via upload

---
+++ Lab10/Lab10/Lab10.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+    int n,m,k,l;
+    do {
+    cout << "Введите количество элементов массива ";
+    cin >> n;
+    } while (n <= 0);
+    int* arr = new int[n];
+    for (int i = 0; i < n; i++)
+        arr[i] = rand() % 100 + 1;
+    cout << "array = ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << "  ";
+    cout << endl;
+    do {
+    cout << "Введите элемент, который нужно удалить ";
+    cin >> m;
+    } while ((m < 0) || (m > n));
+
+    for (int i = m + 1; i < n; ++i) arr[i - 1] = arr[i];
+    n--;
+    cout << "array = ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << "  ";
+    cout << endl;
+    do {
+    cout << "Введите количество заполняемых элементов ";
+    cin >> k;
+    } while (k <= 0);
+    do {
+    cout << "С какого элемента нужно заполнить ";
+    cin >> l;
+    } while ((l < 0) || (l > n));
+    // Создание временного массива для хранения новых елементов
+    int* tmpAddArr = new int[k];
+    // Ввод значений новых елементов
+    cout << "Добавляемые элементы: ";
+    for (int i = 0; i < k; i++) {
+        tmpAddArr[i] = rand() % 100 + 1;
+        cout << tmpAddArr[i] << "  ";
+    }
+    cout << endl;
+    // Создание нового массива, который будет 
+    // содержать елементы начального массива и 
+    // новые елементы массива новых елементов
+    int* newArr = new int[n + k];
+    // Для прохода по массиву новых елементов
+    int count = 0;
+    for (int i = 0; i < n + k; i++) {
+        // Вставляем элементы старого массива в новый
+        if (i < l) {
+            newArr[i] = arr[i];
+        }
+        // Вставляем новые элементы из массива новых элементов
+        else if (i >= l && i < l + k) {
+            newArr[i] = tmpAddArr[count];
+            count++;
+        }
+        // Вставляем элементы старого массива в новый
+        else {
+            newArr[i] = arr[i - count];
+        }
+    }
+    // Удаляем начальный массив
+    delete[] arr;
+    // Удаляем временный массив новых элементов
+    delete[] tmpAddArr;
+    cout << "array = ";
+    for (int i = 0; i < n+k; i++)
+        cout << newArr[i] << "  ";
+    cout << endl;
+    system("pause");
+    return 0;
+}
---
+++ Lab9/Lab9/Lab9.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int k = 0;
+    char f3[255], f4[255];
+    ifstream f1("F1.txt");
+    ofstream f2("F2.txt");
+
+    if (!f1)
+    {
+        cout << "Error!" << endl;
+        system("pause");
+        return 0;
+    }
+
+    while (f1.getline(f3, 255))
+    {
+        k++;
+        if (k > 3)
+        {
+            f2 << f3 << '\n';
+        }
+    }
+
+    f2.close();
+
+    ifstream p2("F2.txt");
+    if (p2)
+    {
+        while (true)
+        {
+            p2 >> f4;
+            if (p2.peek() == '\n')
+                p2.get();
+            if (p2.peek() == EOF)
+                break;
+        }
+        p2.close();
+        cout <<"Last word have "<<strlen(f4) <<" symbols"<< endl;
+    }
+    else
+    {
+        cout << "Error!" << endl;
+        system("pause");
+    }
+
+    system("pause");
+    return 0;
+}
